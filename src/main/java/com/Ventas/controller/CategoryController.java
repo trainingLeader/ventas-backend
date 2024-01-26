fdf2cd20ac9591adc50a932e69bd69694cc1c59f
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-01-26 12:37:39 -0500

    Se agrego Controladores para los end POints

---
+++ src/main/java/com/Ventas/controller/CategoryController.java
@@ -0,0 +1,60 @@
+package com.Ventas.controller;
+
+import com.Ventas.dto.SaveCategory;
+import com.Ventas.persistence.entity.Category;
+import com.Ventas.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<Page<Category>> findAll(Pageable pageable){
+
+        Page<Category> categoriesPage = categoryService.findAll(pageable);
+
+        if(categoriesPage.hasContent()){
+            return ResponseEntity.ok(categoriesPage);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> findOneById(@PathVariable Long categoryId){
+
+        Optional<Category> category = categoryService.findOneById(categoryId);
+
+        return category.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createOne(@RequestBody @Valid SaveCategory saveCategory){
+        Category category = categoryService.createOne(saveCategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<Category> updateOneById(@PathVariable Long categoryId, @RequestBody @Valid SaveCategory saveCategory){
+        Category category = categoryService.updateOneById(categoryId, saveCategory);
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("/{categoryId}/disabled")
+    public ResponseEntity<Category> disableOneById(@PathVariable Long categoryId){
+        Category category = categoryService.disableOneById(categoryId);
+        return ResponseEntity.ok(category);
+    }
+}
---
+++ src/main/java/com/Ventas/controller/ProductController.java
@@ -0,0 +1,60 @@
+package com.Ventas.controller;
+
+import com.Ventas.dto.SaveProduct;
+import com.Ventas.persistence.entity.Product;
+import com.Ventas.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> findAll(Pageable pageable){
+
+        Page<Product> productsPage = productService.findAll(pageable);
+
+        if(productsPage.hasContent()){
+            return ResponseEntity.ok(productsPage);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> findOneById(@PathVariable Long productId){
+
+        Optional<Product> product = productService.findOneById(productId);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createOne(@RequestBody @Valid SaveProduct saveProduct){
+        Product product = productService.createOne(saveProduct);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateOneById(@PathVariable Long productId ,
+                                                 @RequestBody @Valid SaveProduct saveProduct){
+        Product product = productService.updateOneById(productId, saveProduct);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{productId}/disabled")
+    public ResponseEntity<Product> disableOneById(@PathVariable Long productId){
+        Product product = productService.disableOneById(productId);
+        return ResponseEntity.ok(product);
+    }
+}
